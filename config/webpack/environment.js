865ec80456ef30798aab31b9cc6162da4e5c1bc6
loochuynhh <lochuynhhsb@gmail.com>
2024-09-10 12:23:42 +0700

    Finish static pages

---
+++ config/webpack/environment.js
@@ -0,0 +1,8 @@
+const { environment } = require('@rails/webpacker')
+
+environment.config.merge({
+  entry: {
+    application: './app/javascript/application.js'
+  }
+})
+module.exports = environment
