96fb9228b91ca15450d3763c5893820e53168a59
loochuynhh <lochuynhhsb@gmail.com>
2024-09-11 14:30:03 +0700

    Add a full_title helper

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
+  resources :microposts
+  resources :users
+  # root 'static_pages#home'
   get 'static_pages/help'
   get 'static_pages/about'
   get 'static_pages/contact'
+  get 'static_pages/home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -18,4 +21,5 @@ Rails.application.routes.draw do
   # root "application#hello"
   # root 'application#home'
   # get "/bye", to: "application#bye"
+  root "application#hello"
 end
