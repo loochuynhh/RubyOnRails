e3d16fa4000ec960406943b84ec6f648a9620cb4
loochuynhh <lochuynhhsb@gmail.com>
2024-09-12 12:12:14 +0700

    Return to old version and gemfile

---
+++ test/controllers/users_controller_test.rb
@@ -0,0 +1,66 @@
+require 'test_helper'
+
+class UsersControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = users(:one)
+  end
+
+  test 'should get index' do
+    get users_url
+    assert_response :success
+  end
+
+  test 'should get new' do
+    get new_user_url
+    assert_response :success
+  end
+
+  # test "should create user" do
+  #   assert_difference("User.count") do
+  #     post users_url, params: { user: { email: @user.email, name: @user.name } }
+  #   end
+
+  #   assert_redirected_to user_url(User.last)
+  # end
+  # Change default test error
+  test 'should create user' do
+    assert_difference('User.count', 1) do
+      post users_url, params: {user: {email: 'user@example.com', name: 'Example User'}}
+    end
+
+    assert_redirected_to user_url(User.last)
+  end
+
+  test 'should show user' do
+    get user_url(@user)
+    assert_response :success
+  end
+
+  test 'should get edit' do
+    get edit_user_url(@user)
+    assert_response :success
+  end
+
+  # test "should update user" do
+  #   patch user_url(@user), params: { user: { email: @user.email, name: @user.name } }
+  #   assert_redirected_to user_url(@user)
+  # end
+
+  # Change default test error
+  test 'should update user' do
+    patch user_url(@user), params: {user: {email: 'new_email@example.com', name: 'Updated Name'}}
+    assert_redirected_to user_url(@user)
+
+    @user.reload
+    assert_equal 'new_email@example.com', @user.email
+    assert_equal 'Updated Name', @user.name
+  end
+
+  test 'should destroy user' do
+    assert_difference('User.count', -1) do
+      delete user_url(@user)
+    end
+
+    assert_redirected_to users_url
+  end
+end
