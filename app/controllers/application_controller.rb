01a935b939bac75da6f561bbccd010f1da28334c
loochuynhh <lochuynhhsb@gmail.com>
2024-09-13 12:07:10 +0700

    Finish layout and routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   # allow_browser versions: :modern
   def hello
-    render html: "hello, world!"
+    render html: 'hello, world!'
   end
 end
