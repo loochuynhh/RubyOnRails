915454cb016b4e502a9b1f32dfa1ee08c1ba0266
loochuynhh <lochuynhhsb@gmail.com>
2024-09-13 12:07:10 +0700

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/new'
   resources :microposts
   resources :users
+
   root 'static_pages#home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
-  get 'static_pages/home'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  # get "up" => "rails/health#show", as: :rails_health_check
-
-  # Render dynamic PWA files from app/views/pwa/*
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-
-  # Defines the root path route ("/")
-  # root "posts#index"
-  # root "application#hello"
-  # root 'application#home'
-  # get "/bye", to: "application#bye"
+  get '/help', to: 'static_pages#help'
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
+  get '/signup', to: 'users#new', as: 'signup'
 end
