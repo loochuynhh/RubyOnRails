6b558039511835042130671dc450639a74f32160
loochuynhh <lochuynhhsb@gmail.com>
2024-09-18 13:44:41 +0700

    Implement advanced login

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,4 +9,21 @@ class ActiveSupport::TestCase
   def is_logged_in?
     session[:user_id].present?
   end
+
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
+end
+
+class ActionDispatch::IntegrationTest
+  def log_in_as(user, password: 'password', remember_me: '1')
+    params = {
+      session: {
+        email: user.email,
+        password: password,
+        remember_me: remember_me
+      }
+    }
+    post login_path, params: params
+  end
 end
