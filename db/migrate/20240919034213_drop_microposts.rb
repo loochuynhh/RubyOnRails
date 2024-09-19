57085b9792183e181ab734841e4e0928830d74ba
loochuynhh <lochuynhhsb@gmail.com>
2024-09-19 10:50:47 +0700

    Fix some bugs about migrate

---
+++ db/migrate/20240919034213_drop_microposts.rb
@@ -0,0 +1,14 @@
+class DropMicroposts < ActiveRecord::Migration[7.0]
+  def up
+    drop_table :microposts if table_exists?(:microposts)
+  end
+
+  def down
+    create_table :microposts do |t|
+      t.text :content
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
