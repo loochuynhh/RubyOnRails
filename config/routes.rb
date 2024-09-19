a02de7d3b2033eebe6f20f0e6f166359b73b3b35
loochuynhh <lochuynhhsb@gmail.com>
2024-09-19 23:21:12 +0700

    Add to checkout chapter 9

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,10 @@ Rails.application.routes.draw do
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
+
   get '/signup', to: 'users#new', as: 'signup'
+  # get '/edit', to: 'users#edit', as: 'edit'
+
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
